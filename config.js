c15793eca0362e17502916be84a3b9a02107b401
Vitaly Oryschuk <Leilyua@ukr.net>
2020-04-22 14:28:31 +0300

    add config

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 const dbUser = "Pataska";
 const dbPassword = "fhbz8887";
 
-module.exports = {
+const config = {
   port: 8080,
   dbUser,
   dbPassword,
   databaseUrl: `mongodb+srv://${dbUser}:${dbPassword}@clustervitaly-y2ld9.mongodb.net/test?retryWrites=true&w=majority`,
 };
+module.exports = config;
